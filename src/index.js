7d81d48cf2a9e8a7d85a59a9930d0a18d10f0808
Gustavo Pereira <xgustavux@hotmail.com>
2021-12-13 22:49:44 +1000

    Create a React app and publish it to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//import css
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const bookArray = [
+  {
+    id: 1,
+  img: "https://images-na.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg",
+  title: 'Atomic habits',
+  author: 'James Clear'
+},
+{
+  id: 2,
+  img: "https://images-na.ssl-images-amazon.com/images/I/812ufIGbAmL._AC_UL200_SR200,200_.jpg",
+  title: 'The Storyteller: Tales of Life and Music',
+  author: 'Dave Grohl'
+},
+{
+  id: 3,
+  img: "https://images-na.ssl-images-amazon.com/images/I/71fWy4UWqlL._AC_UL200_SR200,200_.jpg",
+  title: 'The Seven Husbands of Evelyn Hugo',
+  author: 'Taylor Jenkins Reid'
+},
+{
+  id: 4,
+  img: "https://images-na.ssl-images-amazon.com/images/I/91YZYvWIhgL._AC_UL200_SR200,200_.jpg",
+  title: 'Apples Never Fall',
+  author: 'Liane Moriarty'
+}
+];
+
+//renderer
+const BookList = () => {
+   return ( 
+     <section className="book-list">
+       {bookArray.map(item => {
+         const { id, img, title, author } = item;
+         return (
+          <Book key={id} item={item} />
+          );
+       })}
+     </section>
+  );
+}
+
+const newFunc = (param) => {
+  console.log(param)
+}
+
+//component
+const Book = (props) => {
+ const { img, title, author } = props.item;
+  return (
+    <div>
+      <img src={img} alt='' />
+      <h4>{title}</h4>
+      <p style={{fontSize:'14px'}}>{author}</p>
+      <button onClick={()=>newFunc(title)}>Button</button>
+    </div>
+  );
+}
+
+
+ReactDom.render(<BookList />, document.getElementById('root'));
