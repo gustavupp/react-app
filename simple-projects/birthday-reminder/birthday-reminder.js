5f20567c50c80cd434415a6b2477530ae6ca2bb7
Gustavo Pereira <93199456+gustavupp@users.noreply.github.com>
2021-12-14 18:41:27 +1000

    Update birthday-reminder.js

--- simple-projects/birthday-reminder/birthday-reminder.js
+++ simple-projects/birthday-reminder/birthday-reminder.js
@@ -1,1 +1,19 @@
 
+import React, { useState } from "react";
+import List from "./List";
+import Data from "./Data";
+
+function BirthdayReminder() {
+const [people, setPeople] = useState(Data);
+return (
+    <main>
+        <section className="container">
+            <h3>{people.length} Birthdays Today</h3>
+            <List props={people} />
+            <button onClick={()=> setPeople([])}>Clear All</button>
+        </section>
+    </main>
+);
+}
+
+export default BirthdayReminder;
